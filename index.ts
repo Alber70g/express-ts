fee4235b349794d65aa150d7ab431f07cb66bd42
Albert Groothedde <albertgroothedde@gmail.com>
2016-05-17 00:02:28 +0200

    WiP: Livereload partly

--- index.ts
+++ index.ts
@@ -1,11 +1,18 @@
 import * as express from 'express';
+const http = require('http');
+const reload = require('reload');
+const path = require('path');
 
-var app = express();
+const app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
+
+const server = http.createServer();
+
+reload(server, app, 2000, 1000);
